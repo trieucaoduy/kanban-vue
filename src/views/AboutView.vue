6bede8bed6f3093fa78bbd6b00c292077ff3a2c2
trieucd <trieucd@runsystem.net>
2024-02-15 14:08:05 +0700

    feat: init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div>About page</div>
+  </main>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
