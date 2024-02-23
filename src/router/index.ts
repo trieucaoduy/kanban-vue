54b4ff0ea8766f1a7be4b623e6aadba6b287d140
trieucd <trieucd@runsystem.net>
2024-02-23 10:18:00 +0700

    feat: update config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../pages/HomeView.vue'
 import Kanban from '../pages/Kanban.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
