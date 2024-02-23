a424921bbdd406776e9f5db23eb6ea5feaa8075a
trieucd <trieucd@runsystem.net>
2024-02-23 16:58:06 +0700

    fix: history router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Kanban from '../pages/Kanban.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
   routes: [
     {
       path: '/',
